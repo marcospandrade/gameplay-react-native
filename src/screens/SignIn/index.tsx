385cd85947d1e47ce037c796c0a56f4518a9f494
Marcos Andrade <marcos.andrade@pitang.com>
2021-06-21 23:51:59 -0300

    splash screen + config inicial

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,21 @@
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+import { styles } from './styles'
+
+export function SignIn() {
+    const [text, setText] = useState('');
+    return (
+        <View style={styles.container}>
+            <Text>Hello World React Native!</Text>
+            {/* <StatusBar style="auto" /> */}
+
+            <TextInput style={styles.input} onChangeText={setText}/>
+
+            <Text>
+                Você digitou: {text}
+            </Text>
+        </View>
+    );
+}
